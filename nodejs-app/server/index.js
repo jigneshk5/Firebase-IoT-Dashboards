4f9b8bfcafc55b6cdd7eb20aeb6f8d472d6161f8
JIGNESH KUMAR <jignesh78bmw@gmail.com>
2020-04-03 15:14:27 +0530

    auth fixed with firebase-admin

--- nodejs-app/server/index.js
+++ nodejs-app/server/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var path = require('path');
+var cookieParser = require('cookie-parser')
 
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -13,20 +12,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//Handle production
-if(process.env.NODE_ENV === 'production' ){
-  app.use(express.static(__dirname+'/public/'));
-  //Handle SPA
-  app.get(/.*/,(req,res) => {
-    res.sendFile(__dirname+'/public/index.html');
-  });
-}
+
 //Set static public data
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/',require('./routes/app'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 // Default response for any other request
@@ -34,7 +25,7 @@ app.use(function(req, res){
   res.status(404).render('404');
 });
 
-const port = 2000;
+const port = process.env.PORT || 3000;
 
 app.listen(port,function(){
   console.log(`server started at port ${port}`);
--- nodejs-app/server/routes/app.js
+++ nodejs-app/server/routes/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Swal = require('sweetalert2');
-//var admin = require("firebase-admin");
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../../firebase-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://iotguy-dashboard.firebaseio.com"
+});
+
 var firebase = require('firebase/app');
 require('firebase/auth');
 require('firebase/database');
@@ -15,83 +22,88 @@ firebase.initializeApp({
     messagingSenderId: "249564343788",
     appId: "1:249564343788:web:b7004b7c8e01eba925a4c9"
 });
-var current;
-firebase.auth().onAuthStateChanged(user =>{
-    current=user;
-});
 
-function isLoggedIn() {
-        // Listen for auth state changes
 
-        //console.log(current);
-        if(current && firebase.auth().currentUser== current){
-            return true;
-        }
-        else{
-            return false;
-        }
-    //console.log(firebase.auth().currentUser);
-}
 router.get('/', (req, res) => {
     res.redirect('/login');
 });
 router.get('/login', (req, res) => {
-    console.log(isLoggedIn());
-    if(isLoggedIn()){
-        res.redirect('/dashboard');
-    }
-    else{
+
         res.render('login',{
             message: ''
         }); 
-    }
+    
 });
 
 router.get('/register', (req, res) => {
 
-    //console.log(isLoggedIn());
-    if(isLoggedIn()){
-        res.redirect('/dashboard');
-    }
-    else{
-        res.render('register',{
-            message: ''
-        }); 
-    }
+    res.render('register',{
+        message: ''
+    });        
+    
+
 });
-router.post('/login', (req, res) => {
-    let email= req.body.email;
-    let password= req.body.password;
-    firebase.auth().signInWithEmailAndPassword(email, password)  // Sign In the user
-    .then(function(result) {
-        //console.log(result);
-        res.redirect('/dashboard');
-      // result.user.tenantId should be ‘TENANT_PROJECT_ID’.
-    }).catch(function(error) {
-      // Handle error.
-        //console.log(error);
-        res.render('login',{
-            message: error.message
-        });
-    }); 
-});
+// router.post('/login', (req, res) => {
+//     let email= req.body.email;
+//     let password= req.body.password;
+//     firebase.auth().signInWithEmailAndPassword(email, password)  // Sign In the user
+//     .then(function(result) {
+//         //console.log(result);
+//         res.redirect('/dashboard');
+//       // result.user.tenantId should be ‘TENANT_PROJECT_ID’.
+//     }).catch(function(error) {
+//       // Handle error.
+//         //console.log(error);
+//         res.render('login',{
+//             message: error.message
+//         });
+//     }); 
+// });
+router.get('/sessionLogin', (req, res) => {
+    // Get the ID token passed and the CSRF token.
+    const idToken = req.query.token.toString();
+    
+    // Set session expiration to 3 hr.
+    const expiresIn = 60 * 60* 3 * 1000;
+    // Create the session cookie. This will also verify the ID token in the process.
+    // The session cookie will have the same claims as the ID token.
+    // To only allow session cookie setting on recent sign-in, auth_time in ID token
+    // can be checked to ensure user was recently signed in before creating a session cookie.
+    admin.auth().createSessionCookie(idToken, {expiresIn})
+      .then((sessionCookie) => {
+       // Set cookie policy for session cookie.
+       const options = {maxAge: expiresIn, httpOnly: true};
+       res.cookie('session', sessionCookie, options);
+
+       res.redirect('/dashboard');
+      })
+      .catch(error => {
+       res.status(401).send('UNAUTHORIZED REQUEST!');
+      });
+  });
 
 router.post('/register', (req, res) => {
     let email= req.body.email;
     let password= req.body.password;
     let confirm_password= req.body.confirm_password;
     if(password===confirm_password){
-    firebase.auth().createUserWithEmailAndPassword(email, password) //Create the user and Sign them In
-    .then(function(result) {
-        res.redirect('/dashboard');
-      // result.user.tenantId should be ‘TENANT_PROJECT_ID’.
-    }).catch(function(error) {
-      // Handle error.
-        //console.log(error);
-        res.render('register',{
-            message: error.message
-        });
-    }); 
+        admin.auth().createUser({
+            email: email,
+            password: password
+          })
+            .then(function(userRecord) {
+              // See the UserRecord reference doc for the contents of userRecord.
+              console.log('Successfully created new user:', userRecord.uid);
+              res.render('login',{
+                message: 'Login with same credentials'
+                }); 
+            })
+            .catch(function(error) {
+              console.log('Error creating new user:', error.errorInfo.message);
+                res.render('register',{
+                    message: error.errorInfo.message
+                });
+            });
     }
     else{
         res.render('register',{
@@ -99,54 +111,49 @@ router.post('/register', (req, res) => {
         });
     }
 });
-router.get('/reset', (req, res) => {
-    res.render('reset',{
-        message: ''
-    });
-});
-router.post('/reset', (req, res) => {
-    let email= req.body.email;
-    firebase.auth().sendPasswordResetEmail(email).then(function() {
-        // Password Reset Email Sent!
-        res.render('reset',{
-            message:'Password Reset Email Sent!'
-        });
-      }).catch(function(error) {
-        res.render('reset',{
-            message: error.message
-        });
-      });
-});
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard',(req, res) => {  //Protected route
     //console.log(firebase.auth().currentUser);
-    if(isLoggedIn()){
-      let user= firebase.auth().currentUser;
-      let userDetail=  {
-            uid: user.uid,
-            displayName : user.displayName,
-            email : user.email,
-            photoURL : user.photoURL,
-            metadata: user.metadata,
-        }
-        firebase.database().ref('users/' + user.uid).update({
-            userName : user.displayName,
-            email : user.email,
-            photoURL : user.photoURL
-          });
+    try{
+        const sessionCookie= req.cookies.session;
+        //console.log(token);
+        // idToken comes from the client app
+        admin.auth().verifySessionCookie(
+            sessionCookie, true /** checkRevoked */)
+            .then((decodedClaims) => {
+                //console.log(decodedClaims);
 
-        res.render('dashboard',userDetail); 
+                firebase.database().ref('users/' + decodedClaims.user_id).update({
+                    email : decodedClaims.email
+                  });
+        
+                res.render('dashboard',{
+                    uid: decodedClaims.user_id,
+                    email: decodedClaims.email,
+                    
+                });
+            })
+            .catch(error => {
+              // Session cookie is unavailable or invalid. Force user to login.
+              console.log(error);
+              res.redirect('/login');
+            });
+        
     }
-    else{
-        res.redirect('/login'); 
+    catch(err){
+        return res.status(401).render('login',{  //401 Unauthorized Accesss
+            message: 'Token expired or tampered'
+        }); 
     }
+    
 });
 
 router.get('/logout', function(req, res) {
-    firebase.auth().signOut();
-    res.redirect('/');
+    res.clearCookie('session');
+    res.redirect('/login');
 });
 
+   
 
 
 module.exports = router;
